49c5002a7edb359993098893f95557e7fd2a7e45
Snakes in the Cloud <snakesinthecloud@gmail.com>
2022-10-28 05:58:12 -0600

    Deploy Flask app to ECS Lambda with CDK and python 3.11.0

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from aws_cdk import App
 from stacks.cdk_ecs_flask.stack import EcsFlaskStack
 from stacks.cdk_fastapi.stack import FastAPIStack
 from stacks.cdk_flask.stack import FlaskStack
+from stacks.cdk_flask_311.stack import FlaskStack311
 
 app = App()
 
-FlaskStack(app, 'CdkPythonFlask')
-FastAPIStack(app, 'CdkPythonFastAPI')
-EcsFlaskStack(app, 'CdkEcsPythonFlask')
+FlaskStack(app, "CdkPythonFlask")
+FastAPIStack(app, "CdkPythonFastAPI")
+EcsFlaskStack(app, "CdkEcsPythonFlask")
+FlaskStack311(app, "CdkPython311Flask")
 
 app.synth()
---
+++ stacks/cdk_flask_311/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Build and run
+IMAGE="cdk_python_flask_311"
+
+echo "Cleaning up any existing containers..."
+docker stop $IMAGE
+docker rm $IMAGE
+
+echo "Building the image..."
+docker build --platform linux/amd64 -t $IMAGE -f Dockerfile .
+echo "Running the container..."
+docker run -dit --platform linux/amd64 --name $IMAGE -p 5001:5001 -p 9000:8080 $IMAGE
+
+# Simulate payload from API Gateway -> Proxy -> Lambda
+echo "Testing..."
+sleep .1
+curl -s -X POST "http://localhost:9000/2015-03-31/functions/function/invocations" -d @tests/test_index.json
+echo ""
+curl -s -X POST "http://localhost:9000/2015-03-31/functions/function/invocations" -d @tests/test_api.json
+echo ""
